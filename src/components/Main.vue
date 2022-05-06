5e3f44a19ea590f6354245f2fb898d6a93293db7
Iqro-riv <arumisokolowska@gmail.com>
2022-05-06 10:07:59 +0200

    feat: uppersection finished

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,7 +2,7 @@
   <div class="relative h-full w-full mobile:top-24">
     <BackgroundSVG class="absolute mobile:bottom-0" />
 
-    <div class="absolute scale-75 mobile:hidden w-full h-full">
+    <div class="absolute scale-75 mobile:hidden w-full h-full top-[3vw]">
       <StarsSVG />
     </div>
 
@@ -10,32 +10,32 @@
 
     <div class="md:absolute flex flex-col">
       <div class="flex flex-row mobile:flex-col-reverse">
-        <div class="mobile:w-full w-[55vw] overflow-hidden">
+        <div
+          class="mobile:w-full w-[55vw] md:relative -top-[5vw] overflow-hidden"
+        >
           <img src="../assets/rocket.png" class="w-full mobile:scale-125" />
         </div>
 
         <div
-          class="w-[45vw] mobile:w-full px-[38px] lg:max-w-[70ch] 4k:max-w-[100ch]"
+          class="flex flex-col gap-4 md:mt-[13vw] lg:mt-[18vw] w-[45vw] mobile:w-full px-[38px] md:max-w-[70ch] h-full"
         >
-          <div class="flex flex-col gap-4 md:mt-[10vw] lg:mt-[15vw]">
-            <span
-              class="md:text-4xl mobile:text-5xl lg:text-5xl 4k:text-7xl text-white font-['Poppins'] font-semibold tracking-tighter"
-            >
-              Gem
-              <span class="text-pink">Launch Platform</span>
-            </span>
+          <span
+            class="md:text-4xl mobile:text-5xl lg:text-5xl 4k:text-7xl text-white font-['Poppins'] font-semibold tracking-tighter"
+          >
+            Gem
+            <span class="text-pink">Launch Platform</span>
+          </span>
 
-            <p
-              class="text-white md:text-xs mobile:text-sm lg:text-sm 4k:text-xl font-['Poppins'] font-normal mobile:w-full w-[75%]"
-            >
-              This exclusive launchpad offers 10set holders the lucrative
-              opportunity to invest in projects with enormous potential at the
-              presale, and allows the ETF portfolio to grow rapidly as Tenset
-              will also invest. These project have the potential to grow 100x
-              and more, as can be seen with our first Gem, Metahero, which
-              experienced 130x in the first few months since launch.
-            </p>
-          </div>
+          <p
+            class="text-white md:text-xs mobile:text-sm lg:text-sm 4k:text-xl font-['Poppins'] font-normal mobile:w-full w-[75%]"
+          >
+            This exclusive launchpad offers 10set holders the lucrative
+            opportunity to invest in projects with enormous potential at the
+            presale, and allows the ETF portfolio to grow rapidly as Tenset will
+            also invest. These project have the potential to grow 100x and more,
+            as can be seen with our first Gem, Metahero, which experienced 130x
+            in the first few months since launch.
+          </p>
         </div>
       </div>
 
